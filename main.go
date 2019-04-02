f78476a98e6cabdd3f432dbcffa874653937ac92
Martin Hermosilla <mhermosi@gmail.com>
2019-04-02 16:18:52 -0400

    Refactoring driver

--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package dockermachinedriverproxmoxve
 
 import (
 	"github.com/docker/machine/libmachine/drivers/plugin"
-	proxmox "github.com/mhermosi/docker-machine-driver-proxmoxve/proxmoxve"
+	proxmoxve "github.com/mhermosi/docker-machine-driver-proxmoxve/proxmoxve"
 )
 
 func main() {
-	plugin.RegisterDriver(proxmox.NewDriver("", ""))
+	plugin.RegisterDriver(proxmoxve.NewDriver("", ""))
 }
